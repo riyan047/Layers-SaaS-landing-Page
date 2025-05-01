b967b4a165ec608da13ade86670fbab19a0e937a
Riyan <griyan115@gmail.com>
2025-05-01 21:21:29 +0530

    responsive navBar completed

--- src/sections/Navbar.tsx
+++ src/sections/Navbar.tsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
 import Logo from '@/assets/images/logo.svg';
 import { MenuIcon } from "lucide-react";
+import Button from "@/components/Button";
+
 
 const navLinks = [
     { label: "Home", href: "#" },
@@ -11,14 +13,23 @@ const navLinks = [
 
 export default function Navbar() {
     return (
-        <section className="py-4 ">
-            <div className="container">
-                <div className="grid grid-cols-2 border border-white/15 rounded-full p-2 px-4 items-center">
+        <section className="py-4 lg:py-8">
+            <div className="container max-w-5xl">
+                <div className="grid grid-cols-2 lg:grid-cols-3 border border-white/15 rounded-full p-2 px-4 md:pr-2 items-center">
                     <div>
-                        <Image src={Logo} alt="Layers logo" className="h-9 w-auto " />
+                        <Image src={Logo} alt="Layers logo" className="h-9 w-auto md:h-auto" />
                     </div>
-                    <div className="flex justify-end md:hidden">
-                        <MenuIcon />
+                    <div className="lg:flex justify-center items-center hidden ">
+                        <nav className="flex gap-6 font-medium">
+                            {navLinks.map(link => (
+                                <a href={link.href} key={link.label}>{link.label}</a>
+                            ))}
+                        </nav>
+                    </div>
+                    <div className="flex justify-end gap-4 ">
+                        <MenuIcon className="md:hidden" />
+                        <Button variant="secondary" className="hidden md:inline-flex items-center" >Log In</Button>
+                        <Button variant="primary" className="hidden md:inline-flex items-center" >Sign Up</Button>
                     </div>
                 </div>
             </div>
