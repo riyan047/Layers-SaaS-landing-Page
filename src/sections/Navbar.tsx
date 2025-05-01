1b0f9ace2ef0a8584f0f08044c48e4b6b76b4a09
Riyan <griyan115@gmail.com>
2025-05-01 20:11:50 +0530

    navBar section

--- src/sections/Navbar.tsx
+++ src/sections/Navbar.tsx
@@ -1,3 +1,7 @@
+import Image from "next/image";
+import Logo from '@/assets/images/logo.svg';
+import { MenuIcon } from "lucide-react";
+
 const navLinks = [
     { label: "Home", href: "#" },
     { label: "Features", href: "#features" },
@@ -6,5 +10,18 @@ const navLinks = [
 ];
 
 export default function Navbar() {
-    return <div>Navbar</div>;
+    return (
+        <section className="py-4 ">
+            <div className="container">
+                <div className="grid grid-cols-2 border border-white/15 rounded-full p-2 px-4 items-center">
+                    <div>
+                        <Image src={Logo} alt="Layers logo" className="h-9 w-auto " />
+                    </div>
+                    <div className="flex justify-end md:hidden">
+                        <MenuIcon />
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
 }
