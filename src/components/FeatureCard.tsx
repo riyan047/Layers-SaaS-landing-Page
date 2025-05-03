5dd5a6f11ce0d15d942e65eb92e885c3cf2a4c1c
Riyan <griyan115@gmail.com>
2025-05-03 18:04:00 +0530

    features section

---
+++ src/components/FeatureCard.tsx
@@ -0,0 +1,26 @@
+import React from "react"
+import { twMerge } from "tailwind-merge";
+
+export default function (props: {
+    title: string;
+    description: string;
+    children?: React.ReactNode;
+    className?: string
+    
+}) {
+
+    const { title, description, children, className } = props
+    return (
+        <div className={twMerge("bg-neutral-900 border border-white/10 p-6 rounded-3xl",className)}>
+            <div className="aspect-video">{children}</div>
+            <div>
+                <h3 className="text-3xl font-medium mt-6">
+                    {title}
+                </h3>
+                <p className="text-white/50">
+                    {description}
+                </p>
+            </div>
+        </div>
+    )
+}
--- src/sections/Features.tsx
+++ src/sections/Features.tsx
@@ -1,3 +1,13 @@
+import FeatureCard from "@/components/FeatureCard";
+import Tag from "@/components/Tag";
+import Avatar1 from "@/assets/images/avatar-ashwin-santiago.jpg"
+import Avatar2 from "@/assets/images/avatar-florence-shaw.jpg"
+import Avatar3 from "@/assets/images/avatar-lula-meyers.jpg"
+import Avatar4 from "@/assets/images/avatar-owen-garcia.jpg"
+import Image from "next/image";
+import Avatar from "@/components/Avatar";
+import Key from "@/components/Key";
+
 const features = [
     "Asset Library",
     "Code Preview",
@@ -9,5 +19,75 @@ const features = [
 ];
 
 export default function Features() {
-    return <div>Features</div>;
+    return (
+        <section className="py-24">
+            <div className="container">
+                <div className="flex justify-center">
+                    <Tag>Features</Tag>
+                </div>
+                <h2 className="text-6xl font-medium text-center mt-6">
+                    Where power meets <span className="text-lime-400">simplicity</span>
+                </h2>
+                <div className="mt-12 grid grid-cols-1 md:grid-cols-4 gap-8">
+                    <FeatureCard
+                        title="Real-time Collaboration"
+                        description="Work together seamlessly with conflict-free team editing"
+                        className="col-span-2"
+                    >
+                        <div className="aspect-video flex items-center justify-center">
+                            <Avatar className="relative z-40">
+                                <Image src={Avatar1} alt="" className="rounded-full" />
+                            </Avatar>
+                            <Avatar className="-ml-6 border-indigo-500 relative z-30">
+                                <Image src={Avatar2} alt="" className="rounded-full" />
+                            </Avatar>
+                            <Avatar className="-ml-6 border-amber-500 relative z-20">
+                                <Image src={Avatar3} alt="" className="rounded-full" />
+                            </Avatar>
+                            <Avatar className="-ml-6 border-transparent">
+                                <div className="size-full bg-neutral-700 rounded-full inline-flex items-center justify-center gap-1">
+                                    {Array.from({ length: 3 }).map((_, i) => (
+                                        <span key={i} className="size-1.5 rounded-full bg-white inline-flex"></span>
+                                    ))}
+                                </div>
+                            </Avatar>
+                        </div>
+
+                    </FeatureCard>
+                    <FeatureCard
+                        title="Interactive Prototyping"
+                        description="Engage your clients with prototypes that react to user actions"
+                        className="col-span-2"
+                    >
+                        <div className="aspect-video flex items-center justify-center">
+                            <p className="text-4xl font-extrabold text-center text-white/20 ">
+                                We&apos;ve achieved <span className="bg-gradient-to-r from bg-purple-400 to-pink-400 bg-clip-text text-transparent">increadible </span>growth this year
+                            </p>
+                        </div>
+                    </FeatureCard>
+                    <FeatureCard
+                        title="Keyboard Quick Actions"
+                        description="Powerful commands to help you create designs more quickly"
+                        className="col-span-2 col-start-2"
+                    >
+                        <div className="aspect-video flex items-center justify-center gap-4">
+                            <Key className="w-28">shift</Key>
+                            <Key>alt</Key>
+                            <Key>C</Key>
+                            
+                        </div>
+                    </FeatureCard>
+
+                </div>
+                <div className="mt-8 flex flex-wrap gap-3 justify-center">
+                    {features.map(feature => (
+                        <div key={feature} className="bg-neutral-900 border border-white/10 inline-flex px-3 py-1.5 rounded-2xl gap-3 items-center">
+                            <span className="text-neutral-950 bg-lime-400 rounded-full size-5 inline-flex justify-center items-center text-xl">&#10038;</span>
+                            <span className="font-medium">{feature}</span>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </section>
+    )
 }
