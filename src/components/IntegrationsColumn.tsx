dc1af950908c09a7ef0a5075d1e3db1ca6a13a6f
Riyan <griyan115@gmail.com>
2025-05-04 01:39:09 +0530

    integrations completed

---
+++ src/components/IntegrationsColumn.tsx
@@ -0,0 +1,30 @@
+import Image from "next/image";
+import { integrationType } from "@/sections/Integrations";
+import { twMerge } from "tailwind-merge";
+
+export default function (props: {
+    integrations: integrationType;
+    className?: string;
+}) {
+
+    const { integrations, className } = props
+    return (
+        <div className={twMerge(" flex flex-col gap-4 pb-4 ", className)}>
+            {integrations.map(integration => (
+                <div key={integration.name}
+                    className="bg-neutral-900 border border-white/10 rounded-3xl p-6 "
+                >
+                    <div className="flex justify-center">
+                        <Image
+                            src={integration.icon}
+                            alt={`${integration.name} icon`}
+                            className="size-24"
+                        />
+                    </div>
+                    <h3 className="text-3xl text-center mt-6 ">{integration.name}</h3>
+                    <p className="text-center text-white/30 mt-2">{integration.description}</p>
+                </div>
+            ))}
+        </div>
+    )
+}
--- src/sections/Integrations.tsx
+++ src/sections/Integrations.tsx
@@ -6,6 +6,7 @@ import slackLogo from "@/assets/images/slack-logo.svg";
 import relumeLogo from "@/assets/images/relume-logo.svg";
 import framerLogo from "@/assets/images/framer-logo.svg";
 import githubLogo from "@/assets/images/github-logo.svg";
+import IntegrationsColumn from "@/components/IntegrationsColumn";
 
 const integrations = [
     { name: "Figma", icon: figmaIcon, description: "Figma is a collaborative interface design tool." },
@@ -16,29 +17,26 @@ const integrations = [
     { name: "GitHub", icon: githubLogo, description: "GitHub is the leading platform for code collaboration." },
 ];
 
+export type integrationType = typeof integrations;
+
 export default function Integrations() {
     return (
-        <section>
-            <div className="py-24 overflow-hidden">
-                <div>
-                    <Tag>Integrations</Tag>
-                </div>
-                <h2 className="text-6xl font-medium mt-6">
-                    Plays well with <span className="text-lime-400">others</span>
-                </h2>
-                <p className="text-white/50 mt-4 text-lg">Layers seamlessly connects with yours favorite tools, makingit easy to plug into any workflow and collaborate across platforms. </p>
-                <div>
-                    {integrations.map(integration => (
-                        <div key={integration.name}>
-                            <div>
-                                <Image
-                                    src={integration.icon}
-                                    alt={`${integration.name} icon`} />
-                            </div>
-                            <h3>{integration.name}</h3>
-                            <p>{integration.description}</p>
+        <section className="py-24 overflow-hidden">
+            <div className="container">
+                <div className="grid lg:grid-cols-2 items-center lg:gap-16 ">
+                    <div>
+                        <Tag>Integrations</Tag>
+                        <h2 className="text-6xl font-medium mt-6">
+                            Plays well with <span className="text-lime-400">others</span>
+                        </h2>
+                        <p className="text-white/50 mt-4 text-lg">Layers seamlessly connects with yours favorite tools, makingit easy to plug into any workflow and collaborate across platforms. </p>
+                    </div>
+                    <div>
+                        <div className="h-[400px] lg:h-[800px] mt-8 lg:mt-0 overflow-hidden grid md:grid-cols-2 gap-4 [mask-image:linear-gradient(to_bottom,transparent,black_10%,black_90%,transparent)]">
+                            <IntegrationsColumn integrations={integrations} />
+                            <IntegrationsColumn integrations={integrations.slice().reverse()} className="hidden md:flex" />
                         </div>
-                    ))}
+                    </div>
                 </div>
             </div>
         </section>
