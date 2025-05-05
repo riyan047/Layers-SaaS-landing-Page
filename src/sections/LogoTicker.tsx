3f604f8d8a19cfeb348da7502c662b7826c85b66
Riyan <griyan115@gmail.com>
2025-05-06 00:34:07 +0530

    logoTicker animation added

--- src/sections/LogoTicker.tsx
+++ src/sections/LogoTicker.tsx
@@ -1,3 +1,4 @@
+"use client";
 import quantumLogo from "@/assets/images/quantum.svg";
 import acmeLogo from "@/assets/images/acme-corp.svg";
 import echoValleyLogo from "@/assets/images/echo-valley.svg";
@@ -6,8 +7,9 @@ import outsideLogo from "@/assets/images/outside.svg";
 import apexLogo from "@/assets/images/apex.svg";
 import celestialLogo from "@/assets/images/celestial.svg";
 import twiceLogo from "@/assets/images/twice.svg";
-import { div } from "framer-motion/client";
+import { easeIn, motion } from "framer-motion";
 import Image from "next/image";
+import { Fragment } from "react";
 
 const logos = [
     { name: "Quantum", image: quantumLogo },
@@ -25,14 +27,26 @@ export default function LogoTicker() {
         <section className="py-24 overflow-x-clip">
             <div className="container">
                 <h3 className="text-center text-white/50 text-xl ">Already chosen by these market Leaders</h3>
-                <div className="overflow-hidden mt-12 [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)]">
-                    <div className="flex flex-row gap-24 pr-24">
-                        {logos.map(logo=>(
-                            <Image src={logo.image}
-                            key={logo.name} 
-                            alt={logo.name}/>
+                <div className=" flex overflow-hidden mt-12 [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)]">
+                    <motion.div
+                        
+                        animate={{ x: "-50%" }}
+                        transition={{
+                            duration: 30,
+                            repeat: Infinity,
+                            ease:"linear"
+                        }}
+                        className="flex flex-none gap-24 pr-24">
+                        {Array.from({ length: 2 }).map((_, i) => (
+                            <Fragment key={i}>
+                                {logos.map(logo => (
+                                    <Image src={logo.image}
+                                        key={logo.name}
+                                        alt={logo.name} />
+                                ))}
+                            </Fragment>
                         ))}
-                    </div>
+                    </motion.div>
                 </div>
             </div>
         </section>
